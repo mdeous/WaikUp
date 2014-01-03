8f8a7cac2bb285d7061ff874eba5f4fa31baa009
Mathieu Deous <mat.deous@gmail.com>
2014-01-03 23:48:37 +0100

    models

---
+++ waikup/models.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+import os
+from datetime import datetime, timedelta
+from hashlib import md5
+
+from peewee import *
+from flask.ext.peewee.admin import ModelAdmin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from waikup import settings
+from waikup.app import db, admin
+
+
+## MODELS
+
+
+class BaseModel(db.Model):
+    update_fields = ()
+    id = PrimaryKeyField()
+
+    def safe_update(self, data):
+        for field in self.update_fields:
+            if (field in data) and (data[field] is not None):
+                setattr(self, field, data[field])
+        return self.save()
+
+
+class User(BaseModel):
+    update_fields = (
+        'username',
+        'first_name',
+        'last_name',
+        'email'
+    )
+    username = CharField(unique=True)
+    first_name = CharField()
+    last_name = CharField()
+    password = CharField()
+    email = CharField()
+    admin = BooleanField(default=False)
+    active = BooleanField(default=True)
+
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password, settings.HASH_METHOD, settings.HASH_SALT_LEN)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def generate_token(self):
+        self.delete_token()
+        return Token.create(user=self)
+
+    def delete_token(self):
+        delete_query = Token.delete().where(Token.user == self)
+        return delete_query.execute()
+
+
+class Token(BaseModel):
+    token = CharField(default=lambda: md5(os.urandom(128)).hexdigest())
+    user = ForeignKeyField(User, related_name='tokens', unique=True)
+    expiry = DateTimeField(default=lambda: datetime.now()+timedelta(weeks=1))
+
+    def __unicode__(self):
+        return u'%s' % self.token
+
+
+class Link(BaseModel):
+    update_fields = (
+        'url',
+        'title',
+        'description'
+    )
+    url = CharField(unique=True)
+    title = CharField()
+    description = TextField(null=True)
+    submitted = DateTimeField(default=datetime.now)
+    author = ForeignKeyField(User, related_name='links')
+
+    def __unicode__(self):
+        return u'%s' % self.url
+
+
+## ADMIN PANEL MODELS
+
+
+class UserAdmin(ModelAdmin):
+    columns = ('username', 'first_name', 'last_name', 'email')
+
+
+class TokenAdmin(ModelAdmin):
+    columns = ('token', 'user')
+    foreign_key_lookups = {'user': 'username'}
+
+
+class LinkAdmin(ModelAdmin):
+    columns = ('url', 'title', 'author')
+    foreign_key_lookups = {'author': 'username'}
+
+
+admin.register(User, UserAdmin)
+admin.register(Token, TokenAdmin)
+admin.register(Link, LinkAdmin)
+admin.setup()
