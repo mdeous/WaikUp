69a96b7f21e42955f323ab24743b0ffd706c66a0
Mathieu Deous <mat.deous@gmail.com>
2014-01-04 18:24:28 +0100

    list/get/create user functions

--- waikup/api/users.py
+++ waikup/api/users.py
@@ -3,7 +3,7 @@
 from flask import Blueprint, request, jsonify
 from peewee import DoesNotExist
 
-from waikup.api import Resource
+from waikup.api import Resource, ResourceSet, login_required
 from waikup.lib.errors import ApiError
 from waikup.lib.helpers import required_fields
 
@@ -43,4 +43,46 @@ def deauth():
         token.delete_instance()
     except DoesNotExist:
         raise ApiError("Invalid token")
+    return jsonify({"success": True})
+
+
+@users.route('/')
+@login_required(admin=True)
+def list_users():
+    from waikup.models import User
+    user_objs = list(User.select())
+    data = ResourceSet(UserResource, user_objs).data
+    return jsonify(data)
+
+
+@users.route('/<int:userid>')
+@login_required(admin=True)
+def get_user(userid):
+    from waikup.models import User
+    try:
+        user = User.get(User.id == userid)
+        data = UserResource(user).data
+    except DoesNotExist:
+        raise ApiError("User not found: %d" % userid, status_code=404)
+    return jsonify(data)
+
+
+@users.route('/', methods=['POST'])
+@login_required(admin=True)
+@required_fields('username', 'first_name', 'last_name', 'email', 'password')
+def create_user():
+    from waikup.models import User
+    user = User.create(
+        username=request.form.get('username'),
+        first_name=request.form.get('first_name'),
+        last_name=request.form.get('last_name'),
+        email=request.form.get('email')
+    )
+    user.set_password(request.form.get('password'))
+    user.save()
+    return jsonify({"success": True})
+
+
+@users.route('/<int:userid>', methods=['PUT'])
+def update_user(userid):
     return jsonify({"success": True})
