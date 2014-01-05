edd7610e3cac6bdaa9c5488914dfbc3742fb5b22
Mathieu Deous <mat.deous@gmail.com>
2014-01-05 15:37:55 +0100

    working web login system

--- waikup/views/webui.py
+++ waikup/views/webui.py
@@ -2,9 +2,13 @@
 
 from flask import Blueprint, render_template
 
+from waikup.lib import globals as g
+
+
 webui = Blueprint('webui', __name__)
 
 
 @webui.route('/')
+@g.auth.login_required
 def index():
     return render_template('index.html')
