113635aef10960e0eb495f263bd5408050bb4290
Mathieu Deous <mat.deous@gmail.com>
2014-01-11 20:20:23 +0100

    remove unused import

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read().strip()
