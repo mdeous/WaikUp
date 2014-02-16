5f2e0c151b706595316066b0dcb64dbfb3e81d54
Mathieu Deous <mat.deous@gmail.com>
2014-02-16 17:25:18 +0100

    allow api calls to be asynchronous

--- waikup/static/js/WaikupAPI.js
+++ waikup/static/js/WaikupAPI.js
@@ -10,28 +10,39 @@ var WaikUp = function(apiURL, apiKey) {
         apiURL = apiURL.substring(0, apiURL.length-1);
     }
 
-    var query = function(methodPath, method, data) {
+    var query = function(methodPath, method, data, callback, async) {
+        // handle default and required arguments
+        if (typeof methodPath === "undefined") {
+            console.log("Missing required argument: methodPath");
+            return null;
+        }
         var endPoint = apiURL + methodPath;
         method = (typeof method === "undefined") ? 'GET' : method;
         data = JSON.stringify((typeof data === "undefined") ? {} : data);
+        callback = (typeof callback === "undefined") ? function(){} : callback;
+        async = (typeof async === "undefined") ? true : async;
+
         var response = $.ajax({
             type: method,
             url: endPoint,
             contentType: (method === "GET") ? "application/x-www-form-urlencoded" : "application/json",
             dataType: "json",
             headers: {"Auth": apiKey},
-            success: function() {},
+            success: callback,
             error: function(xhr, err_desc, err_obj) {
                 console.log("Failed to query "+endPoint+" ("+xhr.statusText+")");
             },
             data: data,
-            async: false
+            async: async
         });
-        return $.parseJSON(response.responseText);
+        if (async) {
+            return $.parseJSON(response.responseText);
+        }
+        return null;
     };
 
-    var topFiveSubmitters = function() {
-        return this.query('/users/top5submitters');
+    var topFiveSubmitters = function(callback) {
+        this.query('/users/top5submitters', 'GET', {}, callback);
     };
 
     return {
