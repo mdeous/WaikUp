f1d291a923ac628b24c76745fcc3d99f81ba9c70
Mathieu Deous <mat.deous@gmail.com>
2014-02-16 16:46:21 +0100

    pass the internal api token in every template context

--- waikup/app.py
+++ waikup/app.py
@@ -98,6 +98,15 @@ def global_forms():
     }
 
 
+@app.context_processor
+def global_variables():
+    from waikup.models import User
+    api_user = User.get(username='waikupapi')
+    return {
+        'internal_api_token': api_user.token.get().token
+    }
+
+
 # Setup custom error handlers
 
 @app.errorhandler(ApiError)
