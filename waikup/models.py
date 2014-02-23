5e6c71148327c0c53a32163e4cc7937643fa13d7
Mathieu Deous <mat.deous@gmail.com>
2014-02-23 16:20:13 +0100

    disable remote debug

--- waikup/models.py
+++ waikup/models.py
@@ -217,8 +217,6 @@ class HybridAuth(Auth):
         def decorator(func):
             @wraps(func)
             def inner(*args, **kwargs):
-                import pydevd
-                pydevd.settrace('90.44.122.204', port=12345, stdoutToServer=True, stderrToServer=True)
                 if request.blueprint in ('links', 'users'):
                     token = self.check_token_header()
                     if not test_func(token.user):
