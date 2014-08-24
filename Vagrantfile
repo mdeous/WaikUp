168ca574f08c2b3fdefa92514fe34f0bfcd1daa4
Mathieu Deous <mat.deous@gmail.com>
2014-08-24 16:55:33 +0200

    use vagrant-fabric plugin

--- Vagrantfile
+++ Vagrantfile
@@ -4,37 +4,12 @@
 # Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
 
-$setup_script = <<SCRIPT
-aptitude update
-aptitude install -y apache2 libapache2-mod-wsgi postgresql libpq-dev python-virtualenv python-dev
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/server.key -out /etc/apache2/server.crt -subj /C=US/ST=City/L=City/O=company/OU=SSLServers/CN=localhost/emailAddress=SSLServer@company.com
-cp /var/www/waikup/src/waikup_apache.conf /etc/apache2/sites-available/default
-cp /var/www/waikup/src/setup_testdb.sql /tmp/waikup_setupdb.sql
-chmod 777 /tmp/waikup_setupdb.sql
-cp /var/www/waikup/src/testdb.sql /tmp/waikup_testdb.sql
-chmod 777 /tmp/waikup_testdb.sql
-su postgres -c "psql --file=/tmp/waikup_setupdb.sql"
-su postgres -c "psql -d waikup --file=/tmp/waikup_testdb.sql"
-cp /var/www/waikup/src/pg_hba.conf /etc/postgresql/9.1/main/
-echo "listen_addresses = '*'" >> /etc/postgresql/9.1/main/postgresql.conf
-echo "host waikup waikup 10.0.2.0/24 password" >> /etc/postgresql/9.1/main/pg_hba.conf
-service postgresql restart
-touch /var/www/waikup/src/.htaccess
-a2enmod ssl
-service apache2 restart
-chmod -R o+w /var/www/waikup
-su vagrant -c "virtualenv /var/www/waikup && \
-source /var/www/waikup/bin/activate && \
-cd /var/www/waikup/src && \
-python setup.py develop && \
-pip install flask-debugtoolbar"
-SCRIPT
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # All Vagrant configuration is done here. The most common configuration
   # options are documented and commented below. For a complete reference,
   # please see the online documentation at vagrantup.com.
-  config.vm.box = "precise32"
-  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.box_url = "https://vagrantcloud.com/ubuntu/trusty64/version/1/provider/virtualbox.box"
   config.vm.network :forwarded_port, guest: 443, host: 8443
   config.vm.network :forwarded_port, guest: 5432, host: 65432
   config.vm.synced_folder ".", "/var/www/waikup/src",
@@ -48,8 +23,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     vb.customize ["modifyvm", :id, "--memory", "512"]
   end
 
-  config.vm.provision :shell do |sh|
-    sh.inline = $setup_script
+  config.vm.provision :fabric do |fabric|
+    fabric.fabfile_path = "./fabfile.py"
+    fabric.tasks = ['setup_all']
   end
 
 end
---
+++ fabfile.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+from fabric.api import *
+
+DEB_REQUIREMENTS = [
+    'apache2',
+    'libapache2-mod-wsgi',
+    'postgresql',
+    'libpq-dev',
+    'python',
+    'python-dev',
+    'openssl',
+    'python-virtualenv'
+]
+SERVICES = [
+    'postgresql',
+    'apache2'
+]
+VIRTUALENV_ACTIVATE = '/var/www/waikup/bin/activate'
+
+
+def start_services():
+    for service in SERVICES:
+        sudo('service %s start' % service)
+
+
+def venv_run(activate, cmd, folder=None):
+    virtualenv = 'source %s' % activate
+    with prefix(virtualenv):
+        with cd(folder or '.'):
+            run(cmd)
+
+
+@task
+def prepare_system():
+    sudo('aptitude update', quiet=True)
+    sudo('aptitude install -y %s' % ' '.join(DEB_REQUIREMENTS), quiet=True)
+    sudo('sysctl -w net.ipv6.conf.all.disable_ipv6=1')
+
+
+@task
+def setup_database():
+    run('cp /var/www/waikup/src/setup_testdb.sql /tmp/waikup_setupdb.sql')
+    run('cp /var/www/waikup/src/testdb.sql /tmp/waikup_testdb.sql')
+    sudo('psql --file=/tmp/waikup_setupdb.sql', user='postgres', quiet=True)
+    sudo('psql -d waikup --file=/tmp/waikup_testdb.sql', user='postgres', quiet=True)
+    sudo('cp /var/www/waikup/src/pg_hba.conf /etc/postgresql/9.3/main/')
+    sudo('echo "listen_addresses = \'*\'" >> /etc/postgresql/9.3/main/postgresql.conf')
+    sudo('echo "host waikup waikup 10.0.2.0/24 password" >> /etc/postgresql/9.3/main/pg_hba.conf')
+    sudo('service postgresql restart')
+
+
+@task
+def setup_apache():
+    sudo('openssl req -x509 -nodes -days 365 -newkey rsa:2048 '
+         '-keyout /etc/apache2/server.key -out /etc/apache2/server.crt '
+         '-subj /C=US/ST=City/L=City/O=company/OU=SSLServers/CN=localhost/emailAddress=SSLServer@company.com',
+         quiet=True
+    )
+    sudo('cp /var/www/waikup/src/waikup_apache.conf /etc/apache2/sites-available/default.conf')
+    sudo('touch /var/www/waikup/.htaccess')
+    sudo('a2enmod ssl')
+    sudo('a2enmod wsgi')
+    sudo('a2dissite 000-default')
+    sudo('a2ensite default')
+    sudo('service apache2 restart')
+
+
+@task
+def setup_waikup():
+    sudo('chmod -R o+w /var/www/waikup')
+    run('virtualenv /var/www/waikup')
+    venv_run(VIRTUALENV_ACTIVATE, 'python /var/www/waikup/src/setup.py develop', folder='/var/www/waikup/src')
+    venv_run(VIRTUALENV_ACTIVATE, 'pip install flask-debugtoolbar')
+
+
+@task(default=True)
+def setup_all():
+    execute(prepare_system)
+    execute(setup_database)
+    execute(setup_apache)
+    execute(setup_waikup)
