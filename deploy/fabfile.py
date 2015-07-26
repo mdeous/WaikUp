1df03e3598b7d67e2cfc707d4185b058102bc881
Mathieu Deous <mat.deous@gmail.com>
2015-07-26 08:48:26 +0200

    move all deployment related files to a dedicated folder

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -28,18 +28,14 @@ def venv_run(activate, cmd, folder=None):
 
 @task
 def prepare_system():
-    sudo('aptitude update', quiet=True)
-    sudo('aptitude install -y %s' % ' '.join(DEB_REQUIREMENTS), quiet=True)
+    sudo('apt-get update', quiet=True)
+    sudo('apt-get install -y %s' % ' '.join(DEB_REQUIREMENTS), quiet=True)
     sudo('sysctl -w net.ipv6.conf.all.disable_ipv6=1')
 
 
 @task
 def setup_database():
-    run('cp /var/www/waikup/src/setup_testdb.sql /tmp/waikup_setupdb.sql')
-    run('cp /var/www/waikup/src/testdb.sql /tmp/waikup_testdb.sql')
-    sudo('psql --file=/tmp/waikup_setupdb.sql', user='postgres', quiet=True)
-    sudo('psql -d waikup --file=/tmp/waikup_testdb.sql', user='postgres', quiet=True)
-    sudo('cp /var/www/waikup/src/pg_hba.conf /etc/postgresql/9.3/main/')
+    sudo('cp /var/www/waikup/src/deploy/pg_hba.conf /etc/postgresql/9.3/main/')
     sudo('echo "listen_addresses = \'*\'" >> /etc/postgresql/9.3/main/postgresql.conf')
     sudo('echo "host waikup waikup 10.0.2.0/24 password" >> /etc/postgresql/9.3/main/pg_hba.conf')
     sudo('service postgresql restart')
@@ -50,9 +46,8 @@ def setup_apache():
     sudo('openssl req -x509 -nodes -days 365 -newkey rsa:2048 '
          '-keyout /etc/apache2/server.key -out /etc/apache2/server.crt '
          '-subj /C=US/ST=City/L=City/O=company/OU=SSLServers/CN=localhost/emailAddress=SSLServer@company.com',
-         quiet=True
-    )
-    sudo('cp /var/www/waikup/src/waikup_apache.conf /etc/apache2/sites-available/default.conf')
+         quiet=True)
+    sudo('cp /var/www/waikup/src/deploy/waikup_apache.conf /etc/apache2/sites-available/default.conf')
     sudo('touch /var/www/waikup/.htaccess')
     sudo('a2enmod ssl')
     sudo('a2enmod wsgi')
@@ -66,13 +61,26 @@ def setup_waikup():
     sudo('chmod -R o+w /var/www/waikup')
     run('virtualenv /var/www/waikup')
     venv_run(VIRTUALENV_ACTIVATE, 'python /var/www/waikup/src/setup.py develop', folder='/var/www/waikup/src')
-    venv_run(VIRTUALENV_ACTIVATE, 'pip install flask-debugtoolbar')
 
 
 @task
 def restart_services():
     for service in SERVICES:
         sudo('service %s restart' % service)
+
+
+@task
+def init_dev():
+    run('cp /var/www/waikup/src/deploy/setup_testdb.sql /tmp/waikup_setupdb.sql')
+    run('cp /var/www/waikup/src/deploy/testdb.sql /tmp/waikup_testdb.sql')
+    sudo('psql --file=/tmp/waikup_setupdb.sql', user='postgres', quiet=True)
+    sudo('psql -d waikup --file=/tmp/waikup_testdb.sql', user='postgres', quiet=True)
+    venv_run(VIRTUALENV_ACTIVATE, 'pip install flask-debugtoolbar')
+
+
+@task
+def init_prod():
+    run("echo 'DEBUG = False' > ")
 
 
 @task(default=True)
